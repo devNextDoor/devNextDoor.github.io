e4b922283ef0596d0efbac090bc069cf0c9c5e0c
solitude79 <koduri1997@gmail.com>
2022-02-11 02:02:20 +0530

    scatter csv input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/modules/material.module';
+import { ScatterResultSheetComponent } from './features/scatter-result-sheet/scatter-result-sheet.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './features/header/header.component';
+import { UserContentInputComponent } from './features/user-content-input/user-content-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScatterResultSheetComponent,
+    HeaderComponent,
+    UserContentInputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
